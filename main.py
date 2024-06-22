8a8364aa58f444183dcb3d1f02bee94aa2ef1e96
Varun Kumar <varuntheace@gmail.com>
2024-06-22 14:14:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -82,6 +82,100 @@ class HotelFareCalculator:
         print("Total food Cost=Rs", self.r, "\n")
 
     def laundrybill(self):
+        print("******LAUNDRY MENU*******")
+
+        print("1.Shorts----->Rs3", "2.Trousers----->Rs4", "3.Shirt--->Rs5", "4.Jeans---->Rs6", "5.Girlsuit--->Rs8",
+              "6.Exit")
+
+        while (1):
+
+            e = int(input("Enter your choice:"))
+
+            if (e == 1):
+                f = int(input("Enter the quantity:"))
+                self.t = self.t + 3 * f
+
+            elif (e == 2):
+                f = int(input("Enter the quantity:"))
+                self.t = self.t + 4 * f
+
+            elif (e == 3):
+                f = int(input("Enter the quantity:"))
+                self.t = self.t + 5 * f
+
+            elif (e == 4):
+                f = int(input("Enter the quantity:"))
+                self.t = self.t + 6 * f
+
+            elif (e == 5):
+                f = int(input("Enter the quantity:"))
+                self.t = self.t + 8 * f
+            elif (e == 6):
+                break;
+            else:
+
+                print("Invalid option")
+
+        print("Total Laundary Cost=Rs", self.t, "\n")
+
+    def gamebill(self):
+        print("******GAME MENU*******")
+
+        print("1.Table tennis----->Rs60", "2.Bowling----->Rs80", "3.Snooker--->Rs70", "4.Video games---->Rs90",
+              "5.Pool--->Rs50==6", "6.Exit")
+
+        while (1):
+
+            g = int(input("Enter your choice:"))
+
+            if (g == 1):
+                h = int(input("No. of hours:"))
+                self.p = self.p + 60 * h
+
+            elif (g == 2):
+                h = int(input("No. of hours:"))
+                self.p = self.p + 80 * h
+
+            elif (g == 3):
+                h = int(input("No. of hours:"))
+                self.p = self.p + 70 * h
+
+            elif (g == 4):
+                h = int(input("No. of hours:"))
+                self.p = self.p + 90 * h
+
+            elif (g == 5):
+                h = int(input("No. of hours:"))
+                self.p = self.p + 50 * h
+            elif (g == 6):
+                break;
+
+            else:
+
+                print("Invalid option")
+
+        print("Total Game Bill=Rs", self.p, "\n")
+
+    def display(self):
+        print("******HOTEL BILL******")
+        print("Customer details:")
+        print("Customer name:", self.name)
+        print("Customer address:", self.address)
+        print("Check in date:", self.cindate)
+        print("Check out date", self.coutdate)
+        print("Room no.", self.rno)
+        print("Your Room rent is:", self.s)
+        print("Your Food bill is:", self.r)
+        print("Your laundary bill is:", self.t)
+        print("Your Game bill is:", self.p)
+
+        self.rt = self.s + self.t + self.p + self.r
+
+        print("Your sub total bill is:", self.rt)
+        print("Additional Service Charges is", self.a)
+        print("Your grandtotal bill is:", self.rt + self.a, "\n")
+        self.rno += 1
+
 
 def main():
     a = HotelFareCalculator()
